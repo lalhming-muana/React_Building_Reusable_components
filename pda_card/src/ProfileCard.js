2b2d3f04c5e1b6fd4626d36b3645a8bb4daf5da4
lhmuana <edd_vai@yahoo.co.in>
2023-12-19 15:20:33 +0530

    props for hming and awmna added

--- pda_card/src/ProfileCard.js
+++ pda_card/src/ProfileCard.js
@@ -1,5 +1,20 @@
-function ProfileCard(){
-    return <div>Profile Card</div>
+function ProfileCard(props){
+
+    /**
+     * Hetah hian hetiang hian props.hming emaw props . hrim hrim hmanglo hian a ziah theih 
+     * 
+     * const hming = props.hming
+     * const awmna = props.awmna
+     * 
+     * Anihloh pawn hetiang hian argument destructuring hmangin a ziah theih bawk
+     * 
+     * const {hming, awmna} = props; 
+     */
+
+    return (<div>
+        <div>{props.hming}</div>
+        <div>{props.awmna}</div>
+    </div>);
 }
 
 export default ProfileCard;
